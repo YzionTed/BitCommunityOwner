53c79652bcae44be3b1b17b0a8a3ce27d218a27c
xiewensheng <912050132@qq.com>
2018-03-06 01:04:13 +0800

    UI卡顿

--- app/src/main/java/com/bit/fuxingwuye/Bluetooth/BluetoothApplication.java
+++ app/src/main/java/com/bit/fuxingwuye/Bluetooth/BluetoothApplication.java
@@ -29,6 +29,9 @@ import com.inuker.bluetooth.library.BluetoothClientManger;
 
 import java.util.ArrayList;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * Created by Dell on 2018/2/10.
  */
@@ -92,6 +95,60 @@ public class BluetoothApplication {
 
     }
 
+    /**
+     * 搜索蓝牙设备
+     *
+     * @stopMinite 搜索多少秒后停止
+     */
+    // 初始化定时器
+    Timer timer;
+
+    public void scanBluetoothDevice(int stopMinite, final CallBack callBack) {
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+
+            @Override
+            public void run() {
+                callBack.onCall(getSearchBlueDeviceBeanList());
+                stopScanning();
+                stopTimer();
+                Log.e("lzp", "timer excute");
+            }
+        }, stopMinite);
+
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
+//                Log.d(TAG, "run: stopLeScan...");
+//
+//            }
+//        }, stopMinite);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
+            scanCallback = initGetCallback();
+            mBluetoothLeScanner.startScan(scanCallback);
+        } else {
+            leScanCallback = leScangetCallback();
+            mBluetoothAdapter.startLeScan(leScanCallback);
+        }
+
+    }
+
+    public interface CallBack {
+        void onCall(ArrayList<SearchBlueDeviceBean> searchBlueDeviceBeanList);
+    }
+
+    // 停止定时器
+    private void stopTimer() {
+        if (timer != null) {
+            timer.cancel();
+            // 一定设置为null，否则定时器不会被回收
+            timer = null;
+        }
+    }
+
+
     @NonNull
     private BluetoothAdapter.LeScanCallback leScangetCallback() {
         return new BluetoothAdapter.LeScanCallback() {
@@ -110,7 +167,7 @@ public class BluetoothApplication {
             @Override
             public void onScanResult(int callbackType, ScanResult result) {
                 super.onScanResult(callbackType, result);
-                Log.d(TAG, "  run: startLeScan  sdk>21  ..."+"UUID=="+result.getDevice().getUuids());
+                Log.d(TAG, "  run: startLeScan  sdk>21  ..." + "UUID==" + result.getDevice().getUuids());
 
                 addScanBloothDevice(result.getDevice(), result.getRssi());
             }
@@ -245,8 +302,6 @@ public class BluetoothApplication {
         }
         return true;
     }
-
-
 
 
     /**
