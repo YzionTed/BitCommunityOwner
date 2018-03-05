05b992bc4168eeecb52edf1b19eddb4696e3df85
bypain <925947408@qq.com>
2018-03-05 14:39:42 +0800

    添加图片缓存策略

--- app/src/main/java/com/bit/fuxingwuye/newsdetail/NewsDetail.java
+++ app/src/main/java/com/bit/fuxingwuye/newsdetail/NewsDetail.java
@@ -24,6 +24,7 @@ import com.alibaba.sdk.android.oss.callback.OSSProgressCallback;
 import com.alibaba.sdk.android.oss.common.auth.OSSCredentialProvider;
 import com.alibaba.sdk.android.oss.common.auth.OSSStsTokenCredentialProvider;
 import com.alibaba.sdk.android.oss.model.PutObjectRequest;
+import com.bit.fuxingwuye.utils.OssManager;
 import com.google.gson.Gson;
 
 import java.sql.Date;
@@ -36,7 +37,6 @@ public class NewsDetail extends AppCompatActivity {
    Intent intent;
    TextView newtitle,time,info,action_bar_title,editorName;
     ImageView img,btn_back;
-    private OSS oss;
     NoticeListBean.RecordsBean bean;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,16 +64,9 @@ public class NewsDetail extends AppCompatActivity {
             @Override
             public void onSuccess(final OssToken data) {
                 if(data!=null){
-                    OSSCredentialProvider credentialProvider = new OSSStsTokenCredentialProvider(data
-                            .getAccessKeyId(), data.getAccessKeySecret(), data.getSecurityToken());
-                    oss = new OSSClient(NewsDetail.this, data.getEndPoint(), credentialProvider);
-                    try {
-                        String url = oss.presignConstrainedObjectURL("bit-app", bean.getThumbnailUrl(),30 * 60);
-//                        ImageLoaderUtil.setImage(url, img,R.mipmap.image_default,R.mipmap.image_default);
-                        GlideUtil.loadImage(NewsDetail.this, url, img);
-                    } catch (ClientException e) {
-                        e.printStackTrace();
-                    }
+                    OssManager.getInstance().init(NewsDetail.this, data);
+                    String url = OssManager.getInstance().getUrl(bean.getThumbnailUrl());
+                    GlideUtil.loadImage(NewsDetail.this, url, img);
                 }
 
             }
